f36a2bc7e9c2ff93b63de7a4b7792f2a71d8b683
root <root@LOK-73477.localiza.corp>
2021-04-27 12:25:59 -0300

    controller code

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,5 +1,5 @@
 
-const { check, validationResult } = require('express-validator');
+// const { check, validationResult } = require('express-validator');
 
 module.exports.formulario_inclusao_noticia = function (app, request, res) {
     res.render('admin/form_add_noticia', { validacao: {}, noticia: {} });
@@ -22,14 +22,4 @@ module.exports.noticias_salvar = function (app, req, res) {
     noticiasModel.salvarNoticia(noticia, function (error, result) {
         res.redirect('/noticias');
     });
-}
-
-module.exports.registerValidator = function () {
-    return [
-        check('titulo', 'Titulo é obrigatório').notEmpty(),
-        check('resumo', 'Resumo é obrigatório').notEmpty(),
-        check('resumo', 'Resumo deve conter entre 10 e 100 caracteres').isLength({ min: 10, max: 100 }),
-        check('autor', 'Autor é obrigatório').notEmpty(),
-        check('data_noticia', 'Data é obrigatório').notEmpty().isDate({ format: 'YYYY-MM-DD' }),
-        check('noticia', 'Noticia é obrigatório').notEmpty()];
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,9 +5,7 @@ module.exports = function (app) {
         app.app.controllers.admin.formulario_inclusao_noticia(app, req, res);
     })
 
-    app.post('/noticias/salvar',
-        app.app.controllers.admin.registerValidator(),
-        function (req, res) {
+    app.post('/noticias/salvar', function (req, res) {
             app.app.controllers.admin.noticias_salvar(app, req, res);
         })
 };
